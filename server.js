cbf288784e7023045b8b1336a4e26951d5a5a96b
Vu Minh Tu <tu.vm41@gmail.com>
2024-09-24 16:03:06 +0700

    Update

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-var express = require('express')
-var ws = require('./ws')
+var server = require('ws').Server
+var s = new server({ port: 5001 })
+CLIENTS=[];
 
-var app = express()
+s.on('connection', function(ws) {
+  CLIENTS.push(ws);
+  console.log('New connection')
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/ws.html');
+  ws.on('message', function(message) {
+    console.log('Received: ', message);
+    sendAll(message);
+  })
+
+  ws.on('close', () => {
+    const clientIndex = CLIENTS.findIndex(it => it == ws);
+    console.log('🚀 ~ ws.on ~ e:', clientIndex)
+    CLIENTS.splice(clientIndex, 1);
+    console.log('🚀 ~ ws.on ~ close:', CLIENTS.length)
+  })
+
+  ws.on('disconnect', () => {
+    console.log('🚀 ~ ws.on ~ disconnect:', CLIENTS.length)
+  })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+function sendExceptCurrent (client, message) {
+  for (var i=0; i<CLIENTS.length; i++) {
+    if (CLIENTS[i] == client) { continue }
+
+    CLIENTS[i].send(message);
+  }
+}
+
+function sendAll (message) {
+  console.log('send message to ', CLIENTS.length, ' client');
+  for (var i=0; i<CLIENTS.length; i++) {
+    try {
+      CLIENTS[i].send(message);
+    } catch(err) {
+      console.log(err);
+      CLIENTS.splice(i, 1);
+    }
+  }
+}
--- ws.js
+++ ws.js
@@ -1,13 +1,17 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+$(document).ready(function() {
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
+  var messageBox = $('#messages-box')
+  var sock       = new WebSocket('ws://localhost:5001')
+  // var sock       = new WebSocket('wss://echo.websocket.org')
+  window.ws      = sock
 
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
-  )
+  sock.onopen    = function(event) { messageBox.prepend('<div class="alert alert-success">Connection opened</div>') }
+  sock.onclose   = function(event) { messageBox.prepend('<div class="alert alert-danger">Connection closed</div>') }
+  sock.onerror   = function(event) { messageBox.prepend('<div class="alert alert-danger">Unknown error</div>'); console.log(event) }
+  sock.onmessage = function(event) { messageBox.prepend('<div class="alert alert-secondary">' + event.data + '</div>'); console.log(event) }
+
+  window.sendMessage = function() {
+    var message = $('#message-input').val()
+    sock.send(message)
+  }
 })
